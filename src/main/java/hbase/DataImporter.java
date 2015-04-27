24947af8cfe7ce8f2f238e46acd66964ebd71ea7
vivilife <pku_huzexing@163.com>
2015-04-27 22:58:42 +0800

    hbase test project

---
+++ src/main/java/hbase/DataImporter.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)DataImporter.java, 2015-4-27. 
+ * 
+ * Copyright 2015 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package hbase;
+
+/**
+ *
+ * @author Administrator
+ *
+ */
+public class DataImporter {
+
+}
